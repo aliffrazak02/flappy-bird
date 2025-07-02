a58e7e94f7155eb4267285ff7ab13999dd8065d9
Aliff Iman <aliffmlg@gmail.com>
2025-07-02 14:41:49 +0800

    Implement pipe generation and movement mechanics for Flappy Bird game

--- flappybird.js
+++ flappybird.js
@@ -18,6 +18,21 @@ let bird = {
     height: birdHeight
 }
 
+//pipes
+let pipeArray = [];
+let pipeWidth = 64;
+let pipeHeight = 512;
+let pipeX = boardWidth;
+let pipeY = 0;
+
+let topPipeImg;
+let bottomPipeImg;
+
+//Physics
+let velocityX = -2; //pipe moving left speed
+
+
+
 window.onload = function() {
     board = document.getElementById("board");
     board.width = boardWidth;
@@ -28,8 +43,17 @@ window.onload = function() {
     birdImg = new Image();
     birdImg.src = "assets/flappybird.png";
     birdImg.onload = function() {
-    context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);}
+        context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+    }
+
+    topPipeImg = new Image();
+    topPipeImg.src = "assets/toppipe.png";
+
+    bottomPipeImg = new Image();
+    bottomPipeImg.src = "assets/bottompipe.png";
+
     requestAnimationFrame(update);
+    setInterval(placePipes, 1500);
 }
 
 function update() {
@@ -38,5 +62,44 @@ function update() {
 
     //bird
     context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
-    
+
+    for (let i = 0; i < pipeArray.length; i++) {
+        let pipe = pipeArray[i];
+        context.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height);
+        pipe.x += velocityX; // Move the pipes to the left
+
+        // Check if the bird has passed the pipe
+        if (!pipe.passed && pipe.x + pipe.width < bird.x) {
+            pipe.passed = true;
+            // Increment score or perform any action when the bird passes a pipe
+        }
+    }
+
+}
+
+function placePipes() {
+
+    let randomPipeY = pipeY - pipeHeight/4 - Math.random() * (pipeHeight/2);
+    let openingSpace = boardHeight/4; // Space between top and bottom pipes
+
+    let topPipe = {
+        img: topPipeImg,
+        x: pipeX,
+        y: randomPipeY,
+        width: pipeWidth,
+        height: pipeHeight,
+        passed: false
+    };
+    pipeArray.push(topPipe);
+
+    let bottomPipe = {
+        img: bottomPipeImg,
+        x: pipeX,
+        y: randomPipeY + pipeHeight + openingSpace,
+        width: pipeWidth,
+        height: pipeHeight,
+        passed: false
+    };
+
+    pipeArray.push(bottomPipe);
 }
